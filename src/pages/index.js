856d73bc6b94f675625ff5216b48eb3b5a20be5c
Shreyan <shreyandasari007@gmail.com>
2025-04-29 21:32:57 +0530

    service flow feature added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,4 @@ export * from "@/pages/iotconnect";
 export * from "@/pages/iotconnectdetail";
 export { default as AboutUs } from "@/pages/about-us";
 export {default as ContactUs} from "@/pages/contact-us";
+export { default as IDEPage } from './IDEPage';
